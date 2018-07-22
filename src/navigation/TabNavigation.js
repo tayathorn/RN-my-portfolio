6c8704ba408be55b6f9e371b79eb9b41d9639427
Ekspan <EksPan@EksPan.local>
2018-07-22 22:55:15 +0700

    + Add Tab icon

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -3,12 +3,27 @@ import {
     View
 } from 'react-native'
 import { createBottomTabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import ProfileStack from '../navigation/ProfileStack'
 import SettingStack from '../navigation/SettingStack'
 
 
 export default createBottomTabNavigator({
-    Profile: ProfileStack,
-    Setting: SettingStack
+    Profile: {
+        screen: ProfileStack,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor, focused }) => (
+                <Icon name="user" size={20} />
+            )
+        }
+    },
+    Setting: {
+        screen: SettingStack,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor, focused }) => (
+                <Icon name="cog" size={20} />
+            )
+        }
+    },
 })
